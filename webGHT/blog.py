cf73edf3b749a5e02db0b0fac9ac2bffc7c158f6
minhvd-WD <92356136+minhVoDuc@users.noreply.github.com>
2023-08-01 11:21:34 +0700

    refactor directory

--- webGHT/blog.py
+++ webGHT/blog.py
@@ -2,8 +2,8 @@ from flask import (
   Blueprint, flash, g, redirect, render_template, request, url_for
 )
 from werkzeug.exceptions import abort
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from webGHT.auth import login_required
+from webGHT.db import get_db
 
 bp = Blueprint('blog', __name__)
 
